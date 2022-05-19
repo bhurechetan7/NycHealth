f30a9921492b975c1c360b01eb35ffe356fddfe1
Praneetha-vattipalli <61209667+Praneetha-vattipalli@users.noreply.github.com>
2022-05-18 21:14:29 -0400

    Update ClinicRepo.java

--- src/main/java/com/nyc/health/Clinic/Repository/ClinicRepo.java
+++ src/main/java/com/nyc/health/Clinic/Repository/ClinicRepo.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ClinicRepo extends JpaRepository<Clinic,Integer>
 {
+ @Query(value = "select * from clinic c where c.start_date=:StartDate AND c.end_date=:EndDate", nativeQuery = true)
+    List<Clinic> findByStartDateAndEndDate(@Param("StartDate") String startDate, @Param("EndDate") String endDate);
 
 
 }

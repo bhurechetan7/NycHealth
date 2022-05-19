e5c43993288860d48449bfc855293976437953c0
Praneetha-vattipalli <61209667+Praneetha-vattipalli@users.noreply.github.com>
2022-05-18 21:10:05 -0400

    Update ClinicRestController.java

--- src/main/java/com/nyc/health/Clinic/Controller/ClinicRestController.java
+++ src/main/java/com/nyc/health/Clinic/Controller/ClinicRestController.java
@@ -1,18 +1,15 @@
 package com.nyc.health.Clinic.Controller;
-import com.fasterxml.jackson.annotation.JacksonInject;
+
 import com.nyc.health.Clinic.EntityClinic.Clinic;
 import com.nyc.health.Clinic.Service.ServiceImpl;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
-import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -31,34 +28,69 @@ public class ClinicRestController {
          // service.getAllClinic();
           return ResponseEntity.status(HttpStatus.OK).headers(headers).body(service.getAllClinic());
      }
+    @GetMapping("/allClosedClinic/{StartDate}/{EndDate}")
+    public ResponseEntity<List<Clinic>> viewAllClosedClinic(@PathVariable String StartDate,@PathVariable String EndDate){
+        String msg = "list of All Closed Clinic";
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("desc","it will give list of all Closed Clinic");
+        // service.getAllClinic();
+        return ResponseEntity.status(HttpStatus.OK).headers(headers).body(service.findByStartDateAndEndDate( StartDate,EndDate));
+    }
+    @GetMapping("/clinic/{id}")
+    public ResponseEntity<Clinic> getClinicById(@PathVariable(value="id") Integer id )
+    {
+        org.springframework.http.HttpHeaders headers = new org.springframework.http.HttpHeaders();
+        headers.add( "dsc", "Record of Clinic with id :" + id);
+        return ResponseEntity.status(HttpStatus.OK).headers(headers).body(service.getClinicById(id));
 
-     @GetMapping("/clinic/{id}")
-      public ResponseEntity<Clinic> getClinicById(@PathVariable(value="id") Integer id )
-           {
-                 HttpHeaders headers = new HttpHeaders();
-                    headers.add( "dsc", "Record of Clinic with id :" + id);
-                   return ResponseEntity.status(HttpStatus.OK).headers(headers).body(service.getClinicById(id));
-
-           }
+    }
 
 
-     @PostMapping("/registerClinic")
-     public ResponseEntity<Clinic> saveClinic(@RequestBody @Valid Clinic newClinic){
-           HttpHeaders headers = new HttpHeaders();
-           headers.add("desc", "It will register new CLinic");
+    @PostMapping("/registerClinic")
+    public ResponseEntity<Clinic> saveClinic(@RequestBody @Valid Clinic newClinic){
+        org.springframework.http.HttpHeaders headers = new org.springframework.http.HttpHeaders();
+        headers.add("desc", "It will register new CLinic");
         // service.registerNewClinic(newClinic);
-           return ResponseEntity.status(HttpStatus.OK).headers(headers).body(service.registerNewClinic(newClinic));
-     }
-      @PutMapping("/updateClinic/{id}")
-     public  ResponseEntity<String> updateClinic(@PathVariable Integer id, @RequestBody @Valid Clinic newClinic){
-                   service.updateClinicRecord(newClinic, id);
-                   return new ResponseEntity<>("Clinic Record with id " + id + " has updated", HttpStatus.OK);
-     }
+        return ResponseEntity.status(HttpStatus.OK).headers(headers).body(service.registerNewClinic(newClinic));
+    }
+    @PutMapping("/updateClinic/{id}")
+    public  ResponseEntity<String> updateClinic(@PathVariable Integer id, @RequestBody @Valid Clinic newClinic){
+        service.updateClinicRecord(newClinic, id);
+        return new ResponseEntity<>("Clinic Record with id " + id + " has updated", HttpStatus.OK);
+    }
 
-     @DeleteMapping("/deleteClinic/{id}")
-     public ResponseEntity<String> deleteClinic(@PathVariable(value="id") Integer id ){
-               service.deleteClinic(id);
-               return new ResponseEntity<>("Clinic Record with id " + id + " has deleted", HttpStatus.OK);
+    @DeleteMapping("/deleteClinic/{id}")
+    public ResponseEntity<String> deleteClinic(@PathVariable(value="id") Integer id){
+        service.deleteClinic(id);
+        return new ResponseEntity<>("Clinic Record with id " + id + " has deleted", HttpStatus.OK);
 
-     }
+    }
+//    @PostMapping("/closeClinic")
+//    public ResponseEntity<String> closeClinic(@PathVariable(value="id")int id){
+//        service.updateClinicByid(id);
+//        return new ResponseEntity<>("Clinic Record with name " + id + " has Closed", HttpStatus.OK);
+//
+//    }
+
+    @PostMapping("/closeClinic")
+    public ResponseEntity<String> CloseClinic(@RequestBody(required = true) Clinic clinic) {
+        Clinic updateClinic = service.getClinicById(clinic.getId());
+        updateClinic.setStatus("close");
+
+        service.updateClinicRecord(updateClinic, clinic.getId());
+        return new ResponseEntity<>("Clinic Record with id " + clinic.getId() + " has updated with status CLOSE", HttpStatus.OK);
+    }
+
+    @PostMapping("/closeClinic/{EndDate}")
+    public ResponseEntity<List<Clinic>> closeAllClinic( ){
+        String msg = "list of All Clinic";
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("desc","it will give list of all closed  Clinic");
+        List<Clinic> c= service.getAllClinic();
+        for(Clinic clinic:c){
+            clinic.setStatus("close");
+        }
+        return ResponseEntity.status(HttpStatus.OK).headers(headers).body(c);
+    }
 }
+
